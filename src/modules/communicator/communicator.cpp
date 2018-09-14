5fd86f6180db124253ba872f0bdd782900a16ea0
Luigi Pannocchi <l.pannocchi@gmail.it>
2018-09-14 17:22:52 +0200

    Added #defines protections

--- src/modules/communicator/communicator.cpp
+++ src/modules/communicator/communicator.cpp
@@ -45,7 +45,9 @@
 
 #include <meas/class_meas.hpp>
 
+#ifdef MEASURE
 extern MEASClass classMeas;
+#endif
 
 const unsigned mode_flag_armed = 128; // following MAVLink spec
 const unsigned mode_flag_custom = 1;
@@ -400,8 +402,12 @@ void Communicator::send_controls(int _actuator_outputs_sub[ORB_MULTI_MAX_INSTANC
 		mavlink_msg_hil_actuator_controls_encode(_vehicle_status.system_id, 200,
 				&message, &hil_act_control);
 		send_mavlink_message(message);
-
+#ifdef MEASURE
 		classMeas.insert_control_timestamp(hil_act_control.time_usec);
+		printf("Ctr time: %llu \n", hil_act_control.time_usec);	
+
+
+#endif
 	}
 }
 
